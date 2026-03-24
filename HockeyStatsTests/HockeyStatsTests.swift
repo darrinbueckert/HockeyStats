cb5f133dea14cdb42fd5d3afad64d2ef13e9037d
Darrin Bueckert <darrin.bueckert@gma>
2026-03-24 16:24:44 -0500

    Initial Commit

---
+++ HockeyStatsTests/HockeyStatsTests.swift
@@ -0,0 +1,16 @@
+//
+//  HockeyStatsTests.swift
+//  HockeyStatsTests
+//
+//  Created by DarrinB on 2026-03-24.
+//
+
+import Testing
+
+struct HockeyStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
